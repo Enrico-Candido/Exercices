eb46cf7513a7cd266ed5cde756db929c38676529
Enrico-Candido <enrico.enr07@gmail.com>
2022-12-15 14:56:58 -0300

    add exs

---
+++ javascript/exListaTarefas/assets/js/main.js
@@ -0,0 +1,76 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', function(e) {
+    //console.log(e); //pegando código da tecla
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+})
+
+function limpaInput() {
+    inputTarefa.value= '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    //botaoApagar.classList.add('apagar');
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'apagar essa tarefa');
+    li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textInput) {
+    const li = criaLi();
+    li.innerText = textInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
+
+btnTarefa.addEventListener('click', function() {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+})
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+})
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas); // conversão para string
+    localStorage.setItem('tarefas', tarefasJSON); // localStorage do navegador, só pode salvar como string nessa 'base'
+    console.log(tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas =  localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas); // convertendo de volta para um objeto JS, voltando a ser um array
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+}
+
+adicionaTarefasSalvas(); // primeiramente foi armazenado no localstorage como string e depois salva novamente como array na ul
