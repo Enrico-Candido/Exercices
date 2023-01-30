f3856066fa431b36f80f532ad6dff7eee9e230a3
Enrico-Candido <enrico.enr07@gmail.com>
2023-01-30 10:07:20 -0300

    add ex

---
+++ csharp/ConApp5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConApp5 {
+    class Program {
+        static void Main(string[] args) {
+            int idFuncionario, horasTrabalhadas;
+            double valorHora, calculoSalario;
+            Console.WriteLine("ID do funcionario:");
+            idFuncionario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Horas trabalhdas do funcionario:");
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor por hora do funcionario");
+            valorHora = double.Parse(Console.ReadLine());
+            calculoSalario = horasTrabalhadas * valorHora;
+            Console.WriteLine("ID funcionario: " + idFuncionario);
+            Console.WriteLine("Salario: U$ " + calculoSalario.ToString("F2"));
+        }
+    }
+}
