69eaccfdfce024467df0ced7a1eb7440300023ef
Enrico-Candido <enrico.enr07@gmail.com>
2023-01-30 09:42:19 -0300

    add ex

---
+++ csharp/ConApp2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConApp2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Primeiro numero a ser somado:");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo numero a ser somado:");
+            int numberTwo = int.Parse(Console.ReadLine());
+            int sum = numberOne + numberTwo;
+            Console.WriteLine("Soma: " + sum);
+        }
+    }
+}
