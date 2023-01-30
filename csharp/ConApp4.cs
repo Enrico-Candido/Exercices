8df851f0f0cc771805f1a188fcf174b7496cb19e
Enrico-Candido <enrico.enr07@gmail.com>
2023-01-30 09:57:28 -0300

    add ex

---
+++ csharp/ConApp4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConApp4 {
+    class Program {
+        static void Main(string[] args) {
+            int numberOne, numberTwo, numberThree, numberFour, diff;
+            Console.WriteLine("Digite o primeiro numero:");
+            numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero:");
+            numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro numero:");
+            numberThree = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o quarto numero:");
+            numberFour = int.Parse(Console.ReadLine());
+            diff = numberOne * numberTwo - numberThree * numberFour;
+            Console.WriteLine("Diferenca: " + diff);
+        }
+    }
+}
