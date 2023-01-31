2612a54745eb13df4da6b82d8156c3a8aadb2bbb
Enrico-Candido <enrico.enr07@gmail.com>
2023-01-31 14:31:53 -0300

    add ex

---
+++ csharp/ExEstruturaCondicional6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExEstruturaCondicional6 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um numero");
+            double number = double.Parse(Console.ReadLine());
+            if (number < 0 || number > 100) Console.WriteLine("Fora do intervalo 0 - 100");
+            else if (number <= 25) Console.WriteLine("[0, 25]");
+            else if (number <= 50) Console.WriteLine("]25, 50]");
+            else if (number <= 75) Console.WriteLine("]50, 75]");
+            else if (number <= 100) Console.WriteLine("]75, 100]");
+        }
+    }
+}
