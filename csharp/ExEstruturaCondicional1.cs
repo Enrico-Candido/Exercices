9d71731ab9f98f851649ab9cc04d5bec9ce63ce0
Enrico-Candido <enrico.enr07@gmail.com>
2023-01-30 21:07:07 -0300

    add exs

---
+++ csharp/ExEstruturaCondicional1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExEstruturaCondicional1 {
+    class Program {
+        static void Main(string[] args) {
+           
+            Console.WriteLine("Digite um numero inteiro");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0) Console.WriteLine("Par");
+            else Console.WriteLine("Impar");
+        }
+    }
+}
