ebdabdd786c34ebf692f461ea397e0a7fbc1d9a5
Enrico-Candido <enrico.enr07@gmail.com>
2023-01-31 13:30:41 -0300

    add ex

---
+++ csharp/ExEstruturaCondicional4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExEstruturaCondicional4 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite a hora inicial e final de um jogo (na mesma linha)");
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(valores[0]);
+            int horaFinal = int.Parse(valores[1]);
+
+            int duracao;
+            if (horaInicial < horaFinal) {
+                duracao = horaFinal - horaInicial;
+            }
+            else {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
