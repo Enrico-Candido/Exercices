b78241537a83d0f0e55be43773a9cf6e8ac68230
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-01 14:01:14 -0300

    add ex

---
+++ csharp/ExEstruturaFor/ExFor5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExFor5 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um numero natural: ");
+            long natural = long.Parse(Console.ReadLine());
+
+            long guarda = 1L;
+            for (long i = natural; i > 0; i--) guarda *= i;
+
+            Console.WriteLine(guarda);
+        }
+    }
+}
