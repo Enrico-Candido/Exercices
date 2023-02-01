9044f5bd00bae3892287f0bd9a9b8ec6eff715ce
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-01 12:21:49 -0300

    add ex

---
+++ csharp/ExEstruturaWhile/ExWhile2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExWhile2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite dois pontos de um plano cartesiano (na mesma linha)");
+
+            string[] valores = Console.ReadLine().Split(' ');
+            double valorX = double.Parse(valores[0]);
+            double valorY = double.Parse(valores[1]);
+
+            while (valorX != 0 && valorY != 0) {
+                if (valorX > 0 && valorY > 0) Console.WriteLine("Primeiro quadrante");
+                else if (valorX < 0 && valorY > 0) Console.WriteLine("Segundo quadrante");
+                else if (valorX < 0 && valorY < 0) Console.WriteLine("Terceiro quadrante");
+                else Console.WriteLine("Quarto quadrante");
+                valores = Console.ReadLine().Split(' ');
+                valorX = double.Parse(valores[0]);
+                valorY = double.Parse(valores[1]);
+            }
+        }
+    }
+}
