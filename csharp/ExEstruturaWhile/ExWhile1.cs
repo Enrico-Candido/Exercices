e749ac597439993a2bc2c7c5ee7f7808ac79651d
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-01 12:21:11 -0300

    add ex

---
+++ csharp/ExEstruturaWhile/ExWhile1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExWhile1 {
+    class Program {
+        static void Main(string[] args) {
+            string senhaCorreta = "2002";
+            string senha = Console.ReadLine();
+
+            while (senha != senhaCorreta) {
+                Console.WriteLine("Senha invalida");
+                senha = Console.ReadLine();
+            }
+            Console.WriteLine("Acesso permitido");
+        }
+    }
+}
