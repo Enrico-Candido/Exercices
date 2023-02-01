91755bb2031dc783533ea6ce6a7cecf318db1519
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-01 12:22:55 -0300

    add ex

---
+++ csharp/ExEstruturaFor/ExFor1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExFor1 {
+    class Proigram {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um valor inteiro no intervalo de [1, 1000]");
+
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 1) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
