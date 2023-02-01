d57ee69a18823afa5e41f71b719a563cc0130a78
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-01 12:39:21 -0300

    add ex

---
+++ csharp/ExEstruturaFor/ExFor2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExFor2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Quantas vezes deseja inserir numeros?");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int x = 0;
+            int y = 0;
+            for (int i = 1; i <= n; i++) {
+                int valor = int.Parse(Console.ReadLine());
+                if (valor >= 10 && valor <= 20) ++x;
+                else ++y;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"{x} in");
+            Console.WriteLine($"{y} out");
+        }
+    }
+}
