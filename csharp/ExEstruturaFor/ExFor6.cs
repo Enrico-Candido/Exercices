8abdb76ecc5a0d154edeedeaf9e708004aef0cb4
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-02 12:43:50 -0300

    add ex

---
+++ csharp/ExEstruturaFor/ExFor6.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExFor6 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um numero inteiro: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++) if (number % i == 0) Console.WriteLine(i);
+        }
+    }
+}
