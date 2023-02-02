6fa257c4fc360646593389ae92b250908fb7461b
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-02 12:44:53 -0300

    add ex

---
+++ csharp/ExEstruturaFor/ExFor7.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExFor7 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um numero natural: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++) Console.WriteLine($"{i} {i*i} {Math.Pow(i, 3)}");
+        }
+    }
+}
