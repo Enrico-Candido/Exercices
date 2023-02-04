9917607befb7df1df712a2156528f548845c2e59
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-04 00:34:22 -0300

    add ex

---
+++ csharp/ExMembrosEstaticos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExMembrosEstaticos1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual e a cotacao do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double qtdeDolares = double.Parse(Console.ReadLine());
+
+            double resultado = ConversorMoeda.Conversao(cotacao, qtdeDolares);
+
+            Console.WriteLine();
+            Console.Write("Valor a ser pago em reais: " + resultado.ToString("F2"));
+        }
+    }
+}
