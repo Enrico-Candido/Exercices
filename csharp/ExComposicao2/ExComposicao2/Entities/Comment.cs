e4dc507bad1ac2b918b64664d65547f2485a6b3a
Enrico-Candido <enrico.enr07@gmail.com>
2023-02-12 18:20:08 -0300

    add ex

---
+++ csharp/ExComposicao2/ExComposicao2/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace ExComposicao2.Entities {
+    class Comment {
+        public string Text { get; set; }
+
+        public Comment() {
+        }
+
+        public Comment(string text) {
+            Text = text;
+        }
+    }
+}
