80cc387df1d1431738eaa49be29d4ce0c35a96c8
Enrico Candido <enrico.enr07@gmail.com>
2023-03-27 13:49:30 -0300

    Adicionando ex de manipulacao de arquivo

---
+++ csharp/ManipulacaoArquivos/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Stream_Writer {
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targerPath = @"c:\temp\file2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targerPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
